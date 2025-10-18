de2ac48f86d25a13ef94d3661a0abdf83bf50e73
Adrian Cisneros <nicolocisneros@gmail.com>
2025-10-17 20:36:01 -0500

    Initial system structure, backend dependencies generated

---
+++ Backend/API-matriculas/src/main/java/com/springback/apimatriculas/ApiMatriculasApplication.java
@@ -0,0 +1,13 @@
+package com.springback.apimatriculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMatriculasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMatriculasApplication.class, args);
+    }
+
+}
